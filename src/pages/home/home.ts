63fbb06834669786400feeb19e8ad52d4d3c860f
vinpc <viniciusamalia@gmail.com>
2020-06-18 20:21:43 -0300

    adicionando a tela signup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -35,6 +35,9 @@ export class HomePage {
     error => {}
     );
    
-   
+  
   }
+  signup() {
+    this.navCtrl.push('SignupPage');
+   }
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -18,8 +13,7 @@ export class SignupPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
-  }
-
+ signupUser(){
+   console.log("enviou o form")
+ }
 }
