8a73988960777e5fea813db5dc666451c283611b
vinpc <viniciusamalia@gmail.com>
2020-06-18 19:40:28 -0300

    criando  a page signup

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StorageService } from '../../app/services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../app/services/domain/cliente.service';
+
+/**
+ * Generated class for the ProfilePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+  cliente: ClienteDTO;
+
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+     public storage : StorageService,
+     public clienteService: ClienteService) {
+  }
+
+  ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email){
+     this.clienteService.findByEmail(localUser.email).subscribe(response =>{
+    this.cliente = response;
+     },
+     error => {});
+    }
+  }
+
+}
