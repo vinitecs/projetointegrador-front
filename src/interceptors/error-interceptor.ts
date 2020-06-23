fcf2b74771cdd236974ec2d2672971cca3dc9cef
vinpc <viniciusamalia@gmail.com>
2020-06-23 19:54:54 -0300

    fazendo alteração

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,11 +1,15 @@
 import {Injectable} from '@angular/core';
 import { HttpEvent,HttpInterceptor,HttpHandler, HTTP_INTERCEPTORS, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { StorageService } from '../app/services/storage.service';
 
 
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
+    constructor(public storage: StorageService){
+
+    }
     intercept (req: HttpRequest<any>, next:HttpHandler): Observable<HttpEvent<any>>{
        console.log("passou no interceptor");
         return next.handle(req)
@@ -19,8 +23,18 @@ export class ErrorInterceptor implements HttpInterceptor{
             }
             console.log("erro detectado pelo interceptor");
             console.log(errorObj);
+            switch(errorObj.status){
+                case 403:
+                    this.handle403();
+                    break;
+
+            }
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handle403(){
+            this.storage.setLocalUser(null);
     }
 }
  export const ErrorInterceptorProvider = {
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -31,7 +31,15 @@ export class ProfilePage {
      this.clienteService.findByEmail(localUser.email).subscribe(response =>{
     this.cliente = response;
      },
-     error => {});
+     error => {
+       if(error.status==403){
+         
+        this.navCtrl.setRoot('HomePage');
+
+       }else{
+         this.navCtrl.setRoot('HomePage');
+       }
+     });
     }
   }
 
