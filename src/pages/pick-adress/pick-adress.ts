79caa19706ddb59d54c852ea8884a1f467cec7e4
vinpc <viniciusamalia@gmail.com>
2020-07-19 11:05:45 -0300

    adicionando dados do pedido

--- src/pages/pick-adress/pick-adress.ts
+++ src/pages/pick-adress/pick-adress.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 
 
 
@@ -11,46 +15,49 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 })
 export class PickAdressPage {
   items:EnderecoDTO[];
+  pedido:PedidoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService,
+    public cartservice: CartService
+
+    ) {
   }
 
   ionViewDidLoad() {
    
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
-          }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "405",
-        complemento: null,
-        bairro: "Centro",
-        cep: "88933822",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
-          }
-        }
-      }
-    ];
-  }
+    let localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.items= response['enderecos'];
 
+          let cart = this.cartservice.getCart();
+
+
+          this.pedido ={
+            cliente:{id:response['id']},
+            enderecoDeEntrega:null,
+            pagamento:null,
+            itens: cart.items.map(x => {return {quantidade:x.quantidade, produto:{id: x.produto.id}}})
+            
+          }
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }
+  }
+nextPage(item: EnderecoDTO){
+  this.pedido.enderecoDeEntrega = {id: item.id};
+  console.log(this.pedido)
+}
 }
