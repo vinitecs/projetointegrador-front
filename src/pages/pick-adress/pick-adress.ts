528ed76b4cfcfd01231cec1976ecffa98516a222
vinpc <viniciusamalia@gmail.com>
2020-08-01 16:19:16 -0300

    criando o tipo de pagamento

--- src/pages/pick-adress/pick-adress.ts
+++ src/pages/pick-adress/pick-adress.ts
@@ -58,6 +58,6 @@ export class PickAdressPage {
   }
 nextPage(item: EnderecoDTO){
   this.pedido.enderecoDeEntrega = {id: item.id};
-  console.log(this.pedido)
+ this.navCtrl.push('PaymentPage',{pedido: this.pedido});
 }
 }
