26b3f7df43613c937f3d1f7828ab58006763c262
vinpc <viniciusamalia@gmail.com>
2020-08-04 23:18:29 -0300

    salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 
 
@@ -24,7 +25,8 @@ cliente: ClienteDTO;
     public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService
    ) {
     this.pedido = this.navParams.get('pedido');
   }
@@ -47,5 +49,20 @@ private findEndereco(id: string, list: EnderecoDTO[]): EnderecoDTO {
 }
 total(){
   return this.cartService.total();
+}
+back(){
+  this.navCtrl.setRoot('CartPage')
+}
+checkout(){
+  this.pedidoService.insert(this.pedido)
+  .subscribe(response=>{
+    this.cartService.createOrClearCart();
+    console.log(response.headers.get("location"));
+  },
+  error=>{
+    if(error.status == 403){
+      this.navCtrl.setRoot('HomePage')
+    }
+    });
 }
 }
